0f4ca8a118d332dd3262bd71f15441bd1f32e3ea
Nguyễn Duy Khương <kn134646@gmail.com>
2024-09-01 00:14:07 +0700

    .

--- my-web-book/src/app/component/author/update-author/update-author.component.ts
+++ my-web-book/src/app/component/author/update-author/update-author.component.ts
@@ -35,14 +35,6 @@ import { MatInputModule } from '@angular/material/input';
   styleUrl: './update-author.component.scss'
 })
 export class UpdateAuthorComponent {
-  // @ViewChild('authorNameInput') authorNameInput!: NgModel;
-  // @ViewChild('penNameInput') penNameInput!: NgModel;
-  // @ViewChild('descriptionInput') descriptionInput!: NgModel;
-  @ViewChild('authorNameInput') authorNameInput!: ElementRef<HTMLInputElement>;
-  @ViewChild('penNameInput') penNameInput!: ElementRef<HTMLInputElement>;
-  @ViewChild('descriptionInput') descriptionInput!: ElementRef<HTMLInputElement>;
-
-
   author = {
     id: 0,
     name: "",
@@ -89,136 +81,19 @@ export class UpdateAuthorComponent {
     this.dialogRef.close();
   }
 
-  // updateAuthor(): void {
-  //   if (!this.author.name) {
-  //     this.toastr.error('Tên tác giả không được để trống', 'Thông báo');
-  //     this.focusOnErrorField(this.authorNameInput);
-  //     return;
-  //   }
-
-  //   if (!this.author.pen_name) {
-  //     this.toastr.error('Bút danh không được để trống', 'Thông báo');
-  //     this.focusOnErrorField(this.penNameInput);
-  //     return;
-  //   }
-
-  //   if (!this.author.bio) {
-  //     this.toastr.error('Mô tả không được để trống', 'Thông báo');
-  //     this.focusOnErrorField(this.descriptionInput);
-  //     return;
-  //   }
-
-  //   if (!this.author.birth_date) {
-  //     this.toastr.error('Ngày sinh không được để trống', 'Thông báo');
-  //     return;
-  //   }
-
-  //   if (!this.selectedCountry) {
-  //     this.toastr.error('Quốc gia không được để trống', 'Thông báo');
-  //     return;
-  //   }
-
-  //   if (!this.imageFile) {
-  //     this.toastr.error('Vui lòng tải lên hình ảnh', 'Thông báo');
-  //     return;
-  //   }
-
-  //   const authorDataCurrent = {
-  //     id: this.author.id,
-  //     name: this.author.name,
-  //     penName: this.author.pen_name,
-  //     description: this.author.bio,
-  //     birthDate: this.author.birth_date,
-  //     selectedCountry: this.selectedCountry,
-  //     status: this.status ? 1 : 0
-  //   };
-
-  //   Swal.fire({
-  //     title: 'Bạn muốn thêm',
-  //     text: 'Thao tác này sẽ không hoàn tác',
-  //     icon: 'warning',
-  //     showCancelButton: true,
-  //     confirmButtonColor: '#3085d6',
-  //     cancelButtonColor: '#d33',
-  //     confirmButtonText: 'Thêm',
-  //     cancelButtonText: 'Thoát',
-  //   }).then((result) => {
-  //     if (result.isConfirmed) {
-  //       this.authorService.updateAuthor(this.author.id, authorDataCurrent, this.imageFile).subscribe(
-  //         (response) => {
-  //           if (response && response.message) {
-  //             if (response.message === 'Update Author successful!') {
-  //               this.toastr.success('Cập nhật thành công', 'Thông báo');
-  //               this.dialogRef.close("updateAuthor");
-  //             } else {
-  //               this.toastr.error('Đã xảy ra lỗi', 'Thông báo');
-  //             }
-  //           } else {
-  //             this.toastr.error('Đã xảy ra lỗi', 'Thông báo');
-  //           }
-  //         },
-  //         error => {
-  //           this.toastr.error('Đã xảy ra lỗi', 'Thông báo');
-  //         }
-  //       );
-  //     }
-  //   })
-  // }
-
   updateAuthor(): void {
-    // if (!this.author.name) {
-    //   this.toastr.error('Tên tác giả không được để trống', 'Thông báo');
-    //   setTimeout(() => {
-    //     const inputElement = document.querySelector('#authorNameInput') as HTMLInputElement;
-    //     if (inputElement) {
-    //       inputElement.focus();
-    //     }
-    //   }, 100);
-    //   return;
-    // }
-
-    // if (!this.author.pen_name) {
-    //   this.toastr.error('Bút danh không được để trống', 'Thông báo');
-    //   setTimeout(() => {
-    //     const inputElement = document.querySelector('#penNameInput') as HTMLInputElement;
-    //     if (inputElement) {
-    //       inputElement.focus();
-    //     }
-    //   }, 100); 
-    //   return;
-    // }
-
-    // if (!this.author.bio) {
-    //   this.toastr.error('Mô tả không được để trống', 'Thông báo');
-    //   setTimeout(() => {
-    //     const inputElement = document.querySelector('#descriptionInput') as HTMLInputElement;
-    //     if (inputElement) {
-    //       inputElement.focus();
-    //     }
-    //   }, 100);
-    //   return;
-    // }
     if (!this.author.name) {
       this.toastr.error('Tên tác giả không được để trống', 'Thông báo');
-      setTimeout(() => {
-        this.authorNameInput.nativeElement.focus();
-      }, 100);
       return;
     }
 
     if (!this.author.pen_name) {
       this.toastr.error('Bút danh không được để trống', 'Thông báo');
-      setTimeout(() => {
-        this.penNameInput.nativeElement.focus();
-      }, 100);
       return;
     }
 
     if (!this.author.bio) {
       this.toastr.error('Mô tả không được để trống', 'Thông báo');
-      setTimeout(() => {
-        this.descriptionInput.nativeElement.focus();
-      }, 100);
       return;
     }
 
@@ -248,13 +123,13 @@ export class UpdateAuthorComponent {
     };
 
     Swal.fire({
-      title: 'Bạn muốn thêm',
+      title: 'Bạn muốn cập nhật',
       text: 'Thao tác này sẽ không hoàn tác',
       icon: 'warning',
       showCancelButton: true,
       confirmButtonColor: '#3085d6',
       cancelButtonColor: '#d33',
-      confirmButtonText: 'Thêm',
+      confirmButtonText: 'Cập nhật',
       cancelButtonText: 'Thoát',
     }).then((result) => {
       if (result.isConfirmed) {
@@ -278,19 +153,4 @@ export class UpdateAuthorComponent {
       }
     });
   }
-
-  // focusOnErrorField(elementRef: ElementRef): void {
-  //   setTimeout(() => {
-  //     elementRef.nativeElement.focus();
-  //   }, 100);
-  // }
-
-  // focusOnErrorField(field: NgModel): void {
-  //   if (field?.invalid) {
-  //     setTimeout(() => {
-  //       const element = document.querySelector(`[name="${field.name}"]`) as HTMLInputElement;
-  //       element?.focus();
-  //     }, 0);
-  //   }
-  // }
 }
