8ae628fff266a9655a3384b57314268f1904c696
Nguyễn Duy Khương <kn134646@gmail.com>
2024-09-02 08:23:37 +0700

    no message

--- PythonBackEnd/myapp/views.py
+++ PythonBackEnd/myapp/views.py
@@ -4,8 +4,8 @@ from rest_framework.exceptions import ValidationError, NotFound
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.response import Response
 
-from myapp.models import Author, Category
-from myapp.serializers import AuthorSerializer, CategorySerializer
+from myapp.models import Author, Category, Tags
+from myapp.serializers import AuthorSerializer, CategorySerializer, TagsSerializer
 
 
 class ListCreateAuthorView(ListCreateAPIView):
@@ -85,3 +85,44 @@ class UpdateDeleteCategoryView(RetrieveUpdateDestroyAPIView):
 
         category.delete()
         return Response({'message': 'Delete Category successful!'}, status=status.HTTP_200_OK)
+
+
+class ListCreateTagsView(ListCreateAPIView):
+    queryset = Tags.objects.all()
+    serializer_class = TagsSerializer
+
+    def create(self, request, *args, **kwargs):
+        name = request.data.get('name')
+        if Tags.objects.filter(name=name).exists():
+            raise ValidationError({'error': 'Tags with this name already exists.'})
+
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({'message': 'Create a new Tags successful!'}, status=status.HTTP_201_CREATED)
+
+
+class UpdateDeleteTagsView(RetrieveUpdateDestroyAPIView):
+    queryset = Tags.objects.all()
+    serializer_class = TagsSerializer
+
+    def put(self, request, *args, **kwargs):
+        tags = self.get_object()
+        name = request.data.get('name')
+        if Tags.objects.filter(name=name).exclude(id=tags.id).exists():
+            raise ValidationError({'error': 'Tags with this name already exists.'})
+
+        tags.updated_at = timezone.now()  # Cập nhật thời gian hiện tại
+        serializer = self.get_serializer(tags, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({'message': 'Update Tags successful!'}, status=status.HTTP_200_OK)
+
+    def delete(self, request, *args, **kwargs):
+        try:
+            tags = self.get_object()
+        except Tags.DoesNotExist:
+            raise NotFound({'error': 'Tags not found.'})
+
+        tags.delete()
+        return Response({'message': 'Delete Tags successful!'}, status=status.HTTP_200_OK)
