ddf1228899c06609debd9dfd0542b148fc5baf35
Nguyễn Duy Khương <kn134646@gmail.com>
2024-10-01 16:27:36 +0700

    no message

--- my-web-book/src/app/component/home/home.component.ts
+++ my-web-book/src/app/component/home/home.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ComicService } from '../../service/comic.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  public comics: any[] = [];
 
+  constructor(
+    private comicService: ComicService,
+  ) { }
+
+  ngOnInit(): void {
+    this.comicService.getAllComic().subscribe((data) => {
+      this.comics = data;
+    });
+  }
 }
