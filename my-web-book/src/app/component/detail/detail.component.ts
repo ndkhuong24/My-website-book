8d48dd4cc584d010275cb096f3a2dca20b46d3b5
Nguyễn Duy Khương <kn134646@gmail.com>
2024-10-07 15:32:59 +0700

    Refactor detail component and add initialization logic

--- my-web-book/src/app/component/detail/detail.component.ts
+++ my-web-book/src/app/component/detail/detail.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ComicService } from '../../service/comic.service';
 
 @Component({
   selector: 'app-detail',
   standalone: true,
   imports: [],
   templateUrl: './detail.component.html',
-  styleUrl: './detail.component.scss'
+  styleUrls: ['./detail.component.scss']
 })
-export class DetailComponent {
+export class DetailComponent implements OnInit {
+  private comicId: number = 0;
 
+  constructor(
+    private route: ActivatedRoute,
+    private comicService: ComicService
+  ) { }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.comicId = id !== null ? +id : 0;
+
+    this.comicService.getDetailBtComicID(this.comicId).subscribe((response) => {
+      console.log(response.data);
+    });
+
+    this.comicService.getById(this.comicId).subscribe((response) => {
+      console.log(response);
+    });
+
+  }
 }
